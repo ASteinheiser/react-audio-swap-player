2f88838ed7fb2f7bf8580f963d928805e308b567
Andrew Steinheiser <asteinheiser@MLLA5306.magicleap.ds>
2021-03-08 20:44:07 -0700

    create audio player component from react-waveform

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
 
-function App() {
+const AudioPlayer = ({
+  buffer = null,
+  width = 500,
+  height = 100,
+  zoom = 1,
+  color = 'black',
+  onDone = null
+}) => {
+  const canvasRef = useRef(null);
+  const relativeWidth = width * zoom;
+
+  useEffect(() => {
+    if (buffer && canvasRef) {
+      const middle = height / 2;
+
+      const channelData = buffer.getChannelData(0);
+      const step = Math.ceil(channelData.length / relativeWidth);
+
+      const ctx = canvasRef.getDOMNode().getContext('2d');
+      ctx.fillStyle = color;
+      draw(relativeWidth, step, middle, channelData, ctx);
+
+      if (onDone) onDone();
+    }
+  }, [buffer, canvasRef, relativeWidth, height, color, onDone]);
+
+  const draw = (width, step, middle, data, ctx) => {
+    for (let i = 0; i < width; i++) {
+      let min = 1.0;
+      let max = -1.0;
+
+      for (let j = 0; j < step; j++) {
+        let datum = data[(i * step) + j];
+
+        if (datum < min) {
+          min = datum;
+        } else if (datum > max) {
+          max = datum;
+        }
+
+        ctx.fillRect(i, (1 + min) * middle, 1, Math.max(1, (max - min) * middle));
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <canvas
+      ref={canvasRef}
+      width={relativeWidth}
+      height={height}
+    />
+  )
+};
 
-export default App;
+export default AudioPlayer;
