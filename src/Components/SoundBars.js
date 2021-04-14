5079bec2e672470273e7762e4c202e6af6a2d5a1
Andrew Steinheiser <asteinheiser@MLLA5306.magicleap.ds>
2021-04-14 02:49:03 -0700

    use different implementation for drawing sound bars

--- src/Components/SoundBars.js
+++ src/Components/SoundBars.js
@@ -1,69 +1,102 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 
-const STEP_SAMPLE_SIZE = 10;
+const STEP_SAMPLE_SIZE = 2;
 const STEP_WIDTH = 1;
 
 const SoundBars = ({
   buffer = null,
   width = 500,
   height = 100,
-  zoom = 1,
   color = 'black',
   onDone = null,
   onClick = () => {},
 }) => {
   const canvasRef = useRef(null);
-  const relativeWidth = width * zoom;
+  const samples = Math.ceil(width / STEP_SAMPLE_SIZE);
+
+  /**
+   * Filters the AudioBuffer retrieved from an external source
+   * @param {AudioBuffer} audioBuffer the AudioBuffer from drawAudio()
+   * @returns {Array} an array of floating point numbers
+   */
+  const filterData = useCallback(audioBuffer => {
+    const rawData = audioBuffer.getChannelData(0); // We only need to work with one channel of data
+    const blockSize = Math.floor(rawData.length / samples); // the number of samples in each subdivision
+    const filteredData = [];
+    for (let i = 0; i < samples; i++) {
+      let blockStart = blockSize * i; // the location of the first sample in the block
+      let sum = 0;
+      for (let j = 0; j < blockSize; j++) {
+        sum = sum + Math.abs(rawData[blockStart + j]); // find the sum of all the samples in the block
+      }
+      filteredData.push(sum / blockSize); // divide the sum by the block size to get the average
+    }
+    return filteredData;
+  }, []);
+
+  /**
+   * Draws the audio file into a canvas element.
+   * @param {Array} normalizedData The filtered array returned from filterData()
+   * @returns {Array} a normalized array of data
+   */
+  const draw = useCallback(normalizedData => {
+    // set up the canvas
+    let canvas = canvasRef.current;
+    const dpr = window.devicePixelRatio || 1;
+    const padding = 8;
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext("2d");
+    ctx.scale(dpr, dpr);
+    ctx.translate(0, canvas.height / 4);
+
+    // draw the line segments
+    const _width = width / normalizedData.length;
+    for (let i = 0; i < normalizedData.length; i++) {
+      const x = _width * i;
+      let height = normalizedData[i] * canvas.height;
+      if (height < 0) {
+          height = 0;
+      } else if (height > canvas.height / 2) {
+          height = height > canvas.height / 2;
+      }
+      drawLineSegment(ctx, x, height - padding, _width, (i + 1) % 2);
+    }
+  }, []);
+
+  /**
+   * A utility function for drawing our line segments
+   * @param {AudioContext} ctx the audio context 
+   * @param {number} x  the x coordinate of the beginning of the line segment
+   * @param {number} height the desired height of the line segment
+   * @param {number} width the desired width of the line segment
+   * @param {boolean} isEven whether or not the segmented is even-numbered
+   */
+  const drawLineSegment = (ctx, x, height, width, isEven) => {
+    ctx.lineWidth = STEP_WIDTH;
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    height = isEven ? height : -height;
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height);
+    ctx.arc(x + width / 2, height, width / 2, Math.PI, 0, isEven);
+    ctx.lineTo(x + width, 0);
+    ctx.stroke();
+  };
 
   useEffect(() => {
     if (!buffer || !canvasRef) return;
 
-    const middle = height / 2;
-
-    const channelData = buffer.getChannelData(0);
-    const step = Math.ceil(channelData.length / relativeWidth);
-
-    const ctx = canvasRef.current.getContext('2d');
-    ctx.fillStyle = color;
-    draw({
-      data: channelData,
-      width: relativeWidth,
-      step,
-      middle,
-      ctx,
-    });
+    draw(filterData(buffer))
 
     if (onDone) onDone();
-  }, [buffer, canvasRef, relativeWidth, height, color, onDone]);
-
-  const draw = ({ width, step, middle, data, ctx }) => {
-    for (let i = 0; i < width; i++) {
-      let min = 1.0;
-      let max = -1.0;
-
-      for (let j = 0; j < step / STEP_SAMPLE_SIZE; j++) {
-        let datumSum = 0;
-        for (let k = 0; k < STEP_SAMPLE_SIZE; k++) {
-          datumSum += data[(i * step) + j + k];
-        }
-        const datumAvg = datumSum / STEP_SAMPLE_SIZE;
-
-        if (datumAvg < min) {
-          min = datumAvg;
-        } else if (datumAvg > max) {
-          max = datumAvg;
-        }
-
-        ctx.fillRect(i, (1 + min) * middle, STEP_WIDTH, Math.max(1, (max - min) * middle));
-      }
-    }
-  }
+  }, [buffer, canvasRef, onDone, filterData, draw]);
 
   const handleClick = e => {
     e.preventDefault();
     if (!buffer) return;
 
-    const songPositionPercentage = e.nativeEvent.offsetX / relativeWidth;
+    const songPositionPercentage = e.nativeEvent.offsetX / width;
     const second = buffer.duration * songPositionPercentage;
     onClick({ second });
   }
@@ -71,7 +104,7 @@ const SoundBars = ({
   return (
     <canvas
       ref={canvasRef}
-      width={relativeWidth}
+      width={width}
       height={height}
       onClick={handleClick}
     />
